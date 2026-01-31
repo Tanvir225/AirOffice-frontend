2532cfc45bff82d231b96efb9d0280257b20a975
Tanvir225 <tanvirrehman200@gmail.com>
2026-01-31 11:10:18 +0600

    main layout max width has changed

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -5,11 +5,11 @@ import Sidebar from "../Component/Share/Sidebar";
 
 const Main = () => {
     return (
-        <div className="w-full mx-auto p-3 lg:max-w-6xl xl:max-w-7xl grid grid-cols-12 gap-5 h-screen">
-            <div className="col-span-full md:col-span-2">
+        <div className="w-full mx-auto p-2 md:w-full lg:max-w-7xl grid grid-cols-12 gap-5 h-screen">
+            <div className="col-span-full md:col-span-3 lg:col-span-2">
                 <Sidebar></Sidebar>
             </div>
-            <div className="col-span-full md:col-span-10 bg-blue-50 rounded">
+            <div className="col-span-full md:col-span-9 lg:col-span-10 bg-blue-50 rounded">
                 <Outlet></Outlet>
             </div>
         </div>
